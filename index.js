f53bbe52e755acb97790b7195312b241fe90370d
Khaled <khaledbm12@gmail.com>
2023-02-07 09:39:03 +0100

    created grid

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    // div.textContent = `${i+1}`
     div.classList.toggle('grid');
     container.appendChild(div);
 }
