447b6bea39a21885c4a4c6538dbf5bc6a6a15b64
Khaled <khaledbm12@gmail.com>
2023-02-09 09:39:33 +0100

    Fixed the canvas sizing

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+const layout = document.querySelector(".layout");
+const card = document.querySelector(".card");
+const select = document.querySelector(".select");
+const gridSize = document.querySelector(".gridSize");
+const root = document.querySelector(":root");
+const rootStyle = getComputedStyle(root)
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    // div.textContent = `${i+1}`
-    div.classList.toggle('grid');
-    container.appendChild(div);
+for (let i = 0 ; i < rootStyle.getPropertyValue('--canvasSize')**2; i++) {
+  const div = document.createElement("div");
+  div.classList.toggle("grid");
+  container.appendChild(div);
 }
 
+console.log(root.style.getPropertyValue('--canvasSize'));
+
+layout.addEventListener("click", () => {
+  card.style.display = "flex";
+});
+
+
+
+select.addEventListener("click", () => {
+  root.style.setProperty('--canvasSize', gridSize.value);
+  card.style.display = "none";
+  container.innerHTML = ""
+  for (let i = 0; i < gridSize.value**2; i++) {
+    const div = document.createElement("div");
+    div.classList.toggle("grid");
+    container.appendChild(div);
+  }
+});
