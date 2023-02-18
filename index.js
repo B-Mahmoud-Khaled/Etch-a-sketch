89769b0035da2b008c890a50fd101628aa24fcf5
Khaled <khaledbm12@gmail.com>
2023-02-18 15:54:50 +0100

    Modified styles and testing

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const card = document.querySelector(".card");
 const select = document.querySelector(".select");
 const gridSize = document.querySelector(".gridSize");
 const root = document.querySelector(":root");
-const rootStyle = getComputedStyle(root)
+const grid = document.querySelector(".grid");
+const rootStyle = getComputedStyle(root);
 
-for (let i = 0 ; i < rootStyle.getPropertyValue('--canvasSize')**2; i++) {
-  const div = document.createElement("div");
-  div.classList.toggle("grid");
-  container.appendChild(div);
-}
-
-console.log(root.style.getPropertyValue('--canvasSize'));
+window.onload = () => {
+	for (let i = 0; i < rootStyle.getPropertyValue("--canvasSize") ** 2; i++) {
+		const div = document.createElement("div");
+		div.classList.add("grid");
+		container.appendChild(div);
+	}
+};
 
 layout.addEventListener("click", () => {
-  card.style.display = "flex";
+	card.style.display = "flex";
 });
-
-
 
 select.addEventListener("click", () => {
-  root.style.setProperty('--canvasSize', gridSize.value);
-  card.style.display = "none";
-  container.innerHTML = ""
-  for (let i = 0; i < gridSize.value**2; i++) {
-    const div = document.createElement("div");
-    div.classList.toggle("grid");
-    container.appendChild(div);
-  }
+	root.style.setProperty("--canvasSize", gridSize.value);
+	card.style.display = "none";
+	container.innerHTML = "";
+	for (let i = 0; i < gridSize.value ** 2; i++) {
+		const div = document.createElement("div");
+		div.classList.add("grid");
+		container.appendChild(div);
+	}
 });
+
+grid.addEventListener('click', () => grid.style.backgroundColor = "black" );
